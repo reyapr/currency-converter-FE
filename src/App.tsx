f625bcce532b1eb0656b969ecd5b0bb6b9c81497
Renal Apriansyah <55680360+reyaprtiket@users.noreply.github.com>
2021-06-24 18:54:22 +0700

    init with CRA

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './routes/Home';
+
+function App() {
+  return (
+    <div className="App">
+      app
+    </div>
+  );
+}
+
+export default App;
